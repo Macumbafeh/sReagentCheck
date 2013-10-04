3667c42403e9b41f7b45c598ae8ae37d2fa4ed01
Siarkowy <siarkowy6@gmail.com>
2013-10-04 23:01:25 +0200

    Initial stable version 1.0 commit

---
+++ sReagentCheck/core.lua
@@ -0,0 +1,30 @@
+--------------------------------------------------------------------------------
+-- sReagentCheck (c) 2013 by Siarkowy
+-- Released under the terms of BSD 2-Clause license.
+--------------------------------------------------------------------------------
+
+local frame = CreateFrame("Frame", "sReagentCheck")
+
+frame:SetScript("OnEvent", function(self, event, ...)
+    local shown
+
+    for id, target in pairs(StealYourCarbonDB.stocklist) do
+        local _, link = GetItemInfo(id)
+        local count = GetItemCount(id)
+        local ratio = count / target
+
+        if ratio <= 0.5 then
+            if not shown then
+                StealYourCarbon:Print("Running out of:")
+                shown = true
+            end
+
+            DEFAULT_CHAT_FRAME:AddMessage(format("   %s %d/%d", link, count,
+                target), 1/ratio, ratio, 0)
+        end
+    end
+
+    self:UnregisterEvent("BAG_UPDATE")
+end)
+
+frame:RegisterEvent("BAG_UPDATE")
